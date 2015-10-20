e4c307981a8cc2c107ae4dfa7dc300a31fa4ba88
pureyang <pureyang@gmail.com>
2015-10-20 10:34:42 -0700

    implemented prettier scrolling intro
    first version, images move to the right position, sorta
    doesn't include rotation or zoom

--- Carousel/ScrollingIntroViewController.swift
+++ Carousel/ScrollingIntroViewController.swift
@@ -12,9 +12,16 @@ class ScrollingIntroViewController: UIViewController, UIScrollViewDelegate {
 
     // orig x 14 y 496 finish x 68 y 893
     @IBOutlet weak var image1View: UIImageView!
-    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var image2View: UIImageView!
+    @IBOutlet weak var image3View: UIImageView!
+    @IBOutlet weak var image4View: UIImageView!
+    @IBOutlet weak var image5View: UIImageView!
+    @IBOutlet weak var image6View: UIImageView!
     
-    var lastContentYOffset = CGFloat(0.0)
+    
+    
+    
+    @IBOutlet weak var scrollView: UIScrollView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,8 +29,15 @@ class ScrollingIntroViewController: UIViewController, UIScrollViewDelegate {
 
         // Do any additional setup after loading the view.
         scrollView.contentSize = CGSize(width: 375, height: 2000)
-
+        
+        NSLog("image1 start %@", NSStringFromCGPoint(image1View.center))
+        NSLog("image2 start %@", NSStringFromCGPoint(image2View.center))
+        NSLog("image3 start %@", NSStringFromCGPoint(image3View.center))
+        NSLog("image4 start %@", NSStringFromCGPoint(image4View.center))
+        NSLog("image5 start %@", NSStringFromCGPoint(image5View.center))
+        NSLog("image6 start %@", NSStringFromCGPoint(image6View.center))
     }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -43,22 +57,53 @@ class ScrollingIntroViewController: UIViewController, UIScrollViewDelegate {
     func scrollViewDidScroll(scrollView: UIScrollView) {
         
 
+        NSLog("%@", NSStringFromCGPoint(scrollView.contentOffset))
+        // scrollview
+        // y = 20 is top
+        // y = 660 is bottom
         // scrolling down
-        /*
-        if (origScrollPoint.y <= scrollView.center.y) {
-            self.image1View.center.y = self.image1View.center.y + 2
-        } else if (origScrollPoint.y > scrollView.center.y) {
-            self.image1View.center.y = self.image1View.center.y - 2
-        }
         
-        */
-        
-        if (self.lastContentYOffset > scrollView.contentOffset.y) {
-            self.image1View.center.y = self.image1View.center.y - 2
-        } else if (self.lastContentYOffset < scrollView.contentOffset.y) {
-            self.image1View.center.y = self.image1View.center.y + 2
+        // image1 start {90.5, 572}
+        // image1 end {149.5, 955}
+
+        // image2 start {220, 574}
+        // image2 end {268, 949}
+
+        // image3 start {292.5, 593}
+        // image3 end {260, 1079}
+
+        // image4 start {188, 610.5}
+        // image4 end {268, 990.5}
+
+        // image5 start {252, 611.5}
+        // image5 end {174, 1098.5}
+
+        // image6 start {337, 612}
+        // image6 end {95, 1098}
+
+        if (scrollView.contentOffset.y <= 660) {
+            let percentScrolled = scrollView.contentOffset.y / 660.0
+            // image1
+            self.image1View.center.y = percentScrolled * 383.0 + 572.0
+            self.image1View.center.x = percentScrolled * 59 + 90.5
+            // TODO rotation
+            // TODO zoom
+            self.image2View.center.y = percentScrolled * 375.0 + 542.0
+            self.image2View.center.x = percentScrolled * -48 + 330.5
+            
+            self.image3View.center.y = percentScrolled * 486.0 + 593.0
+            self.image3View.center.x = percentScrolled * -32.0 + 292.5
+            
+            self.image4View.center.y = percentScrolled * 383.0 + 610.0
+            self.image4View.center.x = percentScrolled * -80 + 350.5
+
+            self.image5View.center.y = percentScrolled * 486.0 + 593.0
+            self.image5View.center.x = percentScrolled * -78 + 252
+
+            self.image6View.center.y = percentScrolled * 486.0 + 593.0
+            self.image6View.center.x = percentScrolled * -242 + 337
         }
-        self.lastContentYOffset = scrollView.contentOffset.y
+
         
     }
     
